1f0846b734a85fda94a8339ba02ce550eca9c81c
Thiago P. Bueno <thiago.pbueno@gmail.com>
2020-04-16 19:05:47 -0300

    feat: add plot kwargs in ExperimentPlotter
    Signed-off-by: Thiago P. Bueno <thiago.pbueno@gmail.com>

--- tuneconfig/plotter.py
+++ tuneconfig/plotter.py
@@ -10,16 +10,26 @@ plt.style.use("seaborn-darkgrid")
 
 
 class ExperimentPlotter:
-    def __init__(self, analysis, *analyses):
+    """ExperimentPlotter manages plotting for multiple experiment analyses.
+
+    Args:
+        analysis (ExperimentAnalysis): (required) analysis object.
+        analyses (List(ExperimentAnalysis): (optional) list of analysis objects.
+    """
+
+    def __init__(self, analysis, *analyses, **kwargs):
         self.analyses = [analysis, *analyses]
         self.grid = TrialGrid(*self.analyses)
+        self.kwargs = kwargs
 
     def plot(self, targets, x_axis=None, y_axis=None, anchors=None):
         trial_grid = self.grid.select(anchors).build(targets, x_axis, y_axis)
 
         nrows, ncols = self.grid.shape
-        fig, axes = plt.subplots(nrows, ncols, squeeze=False)
-        fig.suptitle(", ".join(anchors))
+        fig, axes = plt.subplots(
+            nrows, ncols, squeeze=False, figsize=self.kwargs.get("figsize")
+        )
+        fig.suptitle(", ".join(anchors), fontweight="bold")
 
         for pos, ids, df in self.grid.traverse():
             j, i, y_value, x_value, commonconfig = pos
@@ -38,6 +48,10 @@ class ExperimentPlotter:
         return fig
 
     def _plot(self, ax, df, analysis_id, trial_id, metric):
+        if self.kwargs["plot_type"] == "line":
+            self._plot_line(ax, df, analysis_id, trial_id, metric)
+
+    def _plot_line(self, ax, df, analysis_id, trial_id, metric):
         mean, std = df["mean"], df["std"]
         lower = mean - std
         upper = mean + std
